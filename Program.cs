d431f365c855968c391b9dd035872ed059673d38
manohySr <manohyrajaonah@gmail.com>
2025-01-03 21:57:06 +0330

    Exo3(Array):    moving odd to the front
    I guess It is not the right implementation cause the order is not quite
    right

--- Program.cs
+++ Program.cs
@@ -3,43 +3,74 @@ internal class Program
 {
     public static void Main(string[] args)
     {
-        int[] input = [0, 0, 0, 0];
+
+        int[] input = [4, 1, 3, 2, 6, 5];
+        // excpeted output = [1, 3, 5, 4, 2, 6]
 
         Console.WriteLine("Before");
-        print(input);
+        Print(input);
 
         Console.WriteLine("");
-        moveZeroToEnd(input);
+        MoveOddToFront(input);
+
         Console.WriteLine("");
 
         Console.WriteLine("After");
-        print(input);
+        Print(input);
     }
 
 
-    public static void moveZeroToEnd(int[] input)
+    public static void MoveOddToFront(int[] input)
     {
-        int indexOfZero = 0;
+        // moving odd number to front also means:
+        //    moving odd to front if the next current number is even
 
-        for (int i = 0; i < input.Length; i++)
+
+
+        if (input.Length == 0 || input.Length == 1)
         {
-            if (input[i] != 0)
-            {
-                swapValueInIndex(input, indexOfZero, i);
-                indexOfZero++;
-            }
+            return;
         }
+
+        int i = 1;
+        int indexRef = 0;
+        while (input.Length > i)
+        {
+            if (!IsOdd(input[indexRef]))
+            {
+                if (IsOdd(input[i]))
+                {
+                    SwapValueInIndex(input, indexRef, i);
+                    indexRef++;
+                }
+            }
+            else
+            {
+                indexRef++;
+            }
+            Console.Write("Test: ");
+            Print(input);
+            Console.WriteLine("");
+
+
+            i++;
+        }
+
     }
 
+    public static bool IsOdd(int num)
+    {
+        return num % 2 != 0 ? true : false;
+    }
 
-    public static void swapValueInIndex(int[] input, int indexA, int indexB)
+    public static void SwapValueInIndex(int[] input, int indexA, int indexB)
     {
         int temp = input[indexA];
         input[indexA] = input[indexB];
         input[indexB] = temp;
     }
 
-    public static void print(int[] input)
+    public static void Print(int[] input)
     {
         foreach (var item in input)
         {
@@ -48,27 +79,5 @@ internal class Program
         }
     }
 
-    // Function to generate an int[] array with millions of values
-    public static int[] GenerateInputArray(int size)
-    {
-        // Original small input
-        int[] originalInput = { 0, 1, 0, 3, 12 };
-
-        // Create a new array of the desired size
-        int[] extendedInput = new int[size];
-
-        // Copy original input into the new array
-        Array.Copy(originalInput, extendedInput, originalInput.Length);
-
-        // Fill the rest of the array with a mix of zeros and random non-zero values
-        Random random = new Random();
-        for (int i = originalInput.Length; i < size; i++)
-        {
-            // Randomly assign zero or a non-zero value (1-100)
-            extendedInput[i] = random.Next(0, 2) == 0 ? 0 : random.Next(1, 101);
-        }
-
-        return extendedInput;
-    }
 }
 
